02df55417d8d3adc971d7c1558ef981a599b2bca
dimi88 <kurukii88@gmail.com>
2015-11-24 15:25:18 +0100

    updating the odometer pulses to cm

--- Odometer.cpp
+++ Odometer.cpp
@@ -9,8 +9,8 @@
 /* ---- ODOMETER ---- */
 void updateCounter1(); //ISR for the odometer
 void updateCounter2();
-// Macro to convert from odometer pulses to centimeters. (ca. 40 pulses per meter, that's why we multiply by 2.5 to find value in cm)
-#define PulsesToCentimeters(pulses) ((pulses << 1)/5)
+// Macro to convert from odometer pulses to centimeters. (ca. 33 pulses per meter, that's why we multiply by 3 to find value in cm)
+#define PulsesToCentimeters(pulses) (pulses * 3.3)
 
 volatile unsigned long _pulseCounter[2];
 static unsigned short odometers = 0;
@@ -23,9 +23,9 @@ int Odometer::attach(unsigned short odometerPin){
 	_odometerInterruptPin = digitalPinToInterrupt(odometerPin);
 	if (_odometerInterruptPin != NOT_AN_INTERRUPT){
 		if (!_odometerID){
-			attachInterrupt(_odometerInterruptPin, updateCounter1, CHANGE);
+			attachInterrupt(_odometerInterruptPin, updateCounter1, RISING);
 		}else if (_odometerID == 1){
-			attachInterrupt(_odometerInterruptPin, updateCounter2, CHANGE);
+			attachInterrupt(_odometerInterruptPin, updateCounter2, RISING);
 		}else{
 			return -1; //too many encoders attached
 		}
