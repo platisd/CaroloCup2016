799441eb1c9b56b7404fe2f7b4c735c5aae2551f
dimi88 <kurukii88@gmail.com>
2015-11-29 19:37:55 +0100

    The bare minimum sketch using the library

---
+++ examples/testing/BareMinimum/BareMinimum.ino
@@ -0,0 +1,13 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <CaroloCup.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
